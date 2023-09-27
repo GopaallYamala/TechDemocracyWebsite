7df423926eac7af40abc533a67c38cf5b13c342d
Ramu Vundavelli <rvundavelli@TDC.HYD>
2023-09-27 18:11:32 +0530

    Click-out-side directive/Svg component and nav bar changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,5 +12,77 @@ import { AnimationDefinitions } from 'src/shared/animations';
 export class NavBarComponent {
 
   isMenuCollapsed: boolean = false;
+  displayCreateMenu: boolean = false;
+  solutionsMenu: boolean = false;
+
+  solutionsObj = [{
+    tittle: 'Identity Governance & Administration',
+    desc: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.',
+    stories: 'Success stories',
+    approch: 'Our Approch',
+    customise: 'Customise Solutions',
+  },
+  {
+    tittle: 'Customer Identity & Access Management',
+    desc: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.',
+    stories: 'Success stories',
+    approch: 'Our Approch',
+    customise: 'Customise Solutions',
+  },
+  {
+    tittle: 'Previliged Access Management',
+    desc: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.',
+    stories: 'Success stories',
+    approch: 'Our Approch',
+    customise: 'Customise Solutions',
+  },
+  {
+    tittle: 'Access Management',
+    desc: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.',
+    stories: 'Success stories',
+    approch: 'Our Approch',
+    customise: 'Customise Solutions',
+  }]
+
+  servicesObj = [
+    {
+      tittle: 'Advisory Consulting',
+      desc: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.',
+      businessModel: 'Business Model',
+      stories: 'Customer Success Stories',
+    },
+    {
+      tittle: 'Implementation Service',
+      desc: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.',
+      businessModel: 'Business Model',
+      stories: 'Customer Success Stories',
+    },
+    {
+      tittle: 'Operations & Manage Services',
+      desc: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.',
+      businessModel: 'Business Model',
+      stories: 'Customer Success Stories',
+    },
+  ]
+
+  showMenu(id: string) {
+    let element = document.getElementById(id) as HTMLElement;
+    element.style.visibility = 'visible';
+  }
+
+  hideMenu(id: string) {
+    let element = document.getElementById(id) as HTMLElement;
+    element.style.visibility = 'hidden';
+  }
+
+  showSolutionsNav() {
+    this.solutionsMenu = true;
+    this.displayCreateMenu = false;
+  }
+
+  showServicesNav() {
+    this.solutionsMenu = false;
+    this.displayCreateMenu = true;
+  }
 
 }
