d831553b6319fab62ad3e2126fc43e5ee30f6b6a
Ramu Vundavelli <rvundavelli@TDC.HYD>
2023-10-10 15:12:57 +0530

    Database default URL

--- config/properties.js
+++ config/properties.js
@@ -1,9 +1,5 @@
 module.exports = {
-  PORT : 3000,
-<<<<<<< HEAD
+  PORT: 3000,
   // DB : 'mongodb://localhost:27017/web-site',
-  DB : "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1"
-=======
-  DB : 'mongodb://0.0.0.0:27017/web-site',
->>>>>>> 795350aac8824b6649ea1a9a9e357a1d2c9f272a
-}
+  DB: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.1",
+};
