908c4ce4c3ff1b03eb1e663d9a2b1ef3e7b93cc8
LakshmanKotipalli <lakshman.kotipalli@outlook.com>
2023-10-10 16:17:59 +0530

    solutions page, challenges component, images

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -8,9 +8,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { PotentialDifferentiatorComponent } from "src/app/potential-differentiator/potential-differentiator.component";
 import { ConsultationComponent } from 'src/app/consultation/consultation.component';
 import { AcceleratorsUtilitiesComponent } from 'src/app/accelerators-utilities/accelerators-utilities.component';
+import { ChallengesComponent } from 'src/app/challenges/challenges.component';
+import { ValuedCustomersComponent } from 'src/app/valued-customers/valued-customers.component';
 
 @NgModule({
-  declarations: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent],
+  declarations: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent, ChallengesComponent, ValuedCustomersComponent],
   imports: [
     CommonModule,
     BrowserModule,
@@ -18,7 +20,7 @@ import { AcceleratorsUtilitiesComponent } from 'src/app/accelerators-utilities/a
     RouterModule,
     HttpClientModule
   ],
-  exports: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent]
+  exports: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent, ChallengesComponent, ValuedCustomersComponent]
 })
 
 export class SharedModule {}
