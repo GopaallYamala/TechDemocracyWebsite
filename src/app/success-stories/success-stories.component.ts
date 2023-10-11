fda09b37ff53ba3155056115401cac54a3a1633b
LakshmanKotipalli <lakshman.kotipalli@outlook.com>
2023-10-11 19:41:46 +0530

    Solutions page - UI, Images, multiple component creation

---
+++ src/app/success-stories/success-stories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'success-stories',
+  templateUrl: './success-stories.component.html',
+  styleUrls: ['./success-stories.component.scss']
+})
+
+export class SuccessStoriesComponent {
+
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -10,9 +10,12 @@ import { ConsultationComponent } from 'src/app/consultation/consultation.compone
 import { AcceleratorsUtilitiesComponent } from 'src/app/accelerators-utilities/accelerators-utilities.component';
 import { ChallengesComponent } from 'src/app/challenges/challenges.component';
 import { ValuedCustomersComponent } from 'src/app/valued-customers/valued-customers.component';
+import { SolutionsHeroComponent } from 'src/app/solutions-hero/solutions-hero.component';
+import { SuccessStoriesComponent } from 'src/app/success-stories/success-stories.component';
+import { IGASolutionsComponent } from 'src/app/iga-solutions/iga-solutions.component';
 
 @NgModule({
-  declarations: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent, ChallengesComponent, ValuedCustomersComponent],
+  declarations: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent, ChallengesComponent, ValuedCustomersComponent, SolutionsHeroComponent, SuccessStoriesComponent, IGASolutionsComponent],
   imports: [
     CommonModule,
     BrowserModule,
@@ -20,7 +23,7 @@ import { ValuedCustomersComponent } from 'src/app/valued-customers/valued-custom
     RouterModule,
     HttpClientModule
   ],
-  exports: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent, ChallengesComponent, ValuedCustomersComponent]
+  exports: [PotentialDifferentiatorComponent, ConsultationComponent, AcceleratorsUtilitiesComponent, ChallengesComponent, ValuedCustomersComponent, SolutionsHeroComponent, SuccessStoriesComponent, IGASolutionsComponent]
 })
 
 export class SharedModule {}
