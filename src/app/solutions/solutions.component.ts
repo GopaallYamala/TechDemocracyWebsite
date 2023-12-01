5dcc8518340b1fff1b6ec390e33ef485dbda1a6d
Lakshman Kumar Kotipalli <lakshman.kotipalli@outlook.com>
2023-12-01 14:12:04 +0530

    Solutions - Content updated

--- src/app/solutions/solutions.component.ts
+++ src/app/solutions/solutions.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AnimationDefinitions } from 'src/shared/animations';
 
 @Component({
   selector: 'solutions',
   templateUrl: './solutions.component.html',
-  styleUrls: ['./solutions.component.scss']
+  styleUrls: ['./solutions.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: AnimationDefinitions
 })
 
 export class SolutionsComponent implements OnInit {
