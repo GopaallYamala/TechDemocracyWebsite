12169dd5b51a6b2e122c4fc5565c06f96603fd1a
Lakshman Kumar Kotipalli <lakshman.kotipalli@outlook.com>
2023-12-27 17:05:41 +0530

    Meta fixes and Nav bar changes

--- src/app/company/contact-us/contact-us.component.ts
+++ src/app/company/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Meta } from "@angular/platform-browser";
 
 @Component({
   selector: 'contact-us',
@@ -9,8 +10,16 @@ import { Component, OnInit } from "@angular/core";
 export class ContactUsCompoent implements OnInit {
 
   ngOnInit() {
-
+    // Meta tags starts
+    this.meta.addTags([
+      {name: 'title', content: "Contact Us | Get in Touch with TechDemocracy | Plan a Meeting"},
+      { name: 'description', content: "Connect effortlessly with TechDemocracy’s delivery center in the US, India, Canada, or Philippines using our contact information or the inquiry submission form." },
+      { name: 'keywords', content: "contact information, inquiry form, contact details, inquiry submission, India office, techdemocracy India, Cotelligent, connect with us, TechDemocracy US, TechDemocracy Canada, TechDemocracy Philippines, contact form, get connected, customer support, reach us" }]
+    );
+    // Meta tags ends
   }
+
+  constructor (private meta: Meta) {}
 
 }
 
