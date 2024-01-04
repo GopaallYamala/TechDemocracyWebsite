acbf713fe20d3baa2fb045bda8a3bb2efa8565d2
Ramu Vundavelli <rvundavelli@TDC.HYD>
2024-01-04 21:15:14 +0530

    Node mailer and contact-us toast

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ var bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
 var router = express.Router();
 // call the database connectivity function
 // configure app.use()
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: "*" }));
 app.use(log);
 app.use(bodyParserJSON);
 app.use(bodyParserURLEncoded);
@@ -85,38 +85,51 @@ app.listen(properties.PORT, (req, res) => {
   console.log(`Server is running on ${properties.PORT} port.`);
 });
 
-app.post('/sendmail', (req, res) => {
+app.post("/sendmail", (req, res) => {
   console.log("---- node req came");
   let mailData = req.body;
   console.log(req.body, "-------req body");
-  sendMail(mailData, info => {
+  sendMail(mailData, (info) => {
     console.log(`Mail has been sent :)`);
     res.send(info);
-  })
-})
+  });
+});
 
 async function sendMail(data, callback) {
   // creating reusable object with default smtp transporter
   let transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
+    host: "smtp.office365.com",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
       user: details.email,
-      pass: details.password
-    }
+      pass: details.password,
+    },
   });
 
-  let mailOptions  = {
+  let mailOptions = {
     from: '"TDC Solutions"', // sender address
     to: data.email, // list of receivers
     subject: "Welcome to Cotelligent", // mail subject
     html: `<h1>Hello ${data.fullName}</h1><br>
-    <h3>Thanks for the Message, we will get back to you soon!</h3>`
+    <h3>Thanks for the Message, we will get back to you soon!</h3>`,
+  };
+
+  let mailOptionsToAdminstator = {
+    from: '"TDC Solutions"', // sender address
+    to: "solutions@techdemocracy.com", // list of receivers
+    subject: "Attention Required: New Contact Form Submission", // mail subject
+    html: `<h1>Hi, This is ${data.fullName}</h1><br>
+    <h3>${data.helpText}</h3>`,
   };
 
   // sending mail with defined transporter object
-  let info = await transporter.sendMail(mailOptions);
+  // let info = await transporter.sendMail(mailOptions);
+
+  let info = Promise.all([
+    transporter.sendMail(mailOptions),
+    transporter.sendMail(mailOptionsToAdminstator),
+  ]);
 
   callback(info);
 }
